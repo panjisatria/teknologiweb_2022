478930505ce050c4954f47d1e585930c2a46d3d0
Panji Satria Taqwa Putra Purnama <96733005+panjisatria@users.noreply.github.com>
2022-04-21 10:37:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 Vue.createApp({
   data() {
     return {
+      header: {
+        title: "Panji Satria Taqwa Putra Purnama",
+        description:"Nanti Kita Cerita Tentang Hari Ini",
       message: "Hello Vue!",
           ig: {
             url: "https://instagram.com/pnjstr.id",
             title: "panji",
+          Article: {
+              image:"https://panjisatria.github.io/teknologiweb_2022/WhatsApp%20Image%202022-03-28%20at%2021.02.23.jpeg",
+              title:"panji",
+              heading: "Berikut Model Illustrasi yang saya buat pada Adobe Ilustrator",
+              image:"astrnt.jpg",
     };
   },
 }).mount("#app");
