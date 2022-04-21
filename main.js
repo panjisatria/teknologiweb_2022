67e64003f3c33aab650b2c8a9195e52e1f799753
Panji Satria Taqwa Putra Purnama <96733005+panjisatria@users.noreply.github.com>
2022-04-21 10:05:16 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ Vue.createApp({
   data() {
     return {
       message: "Hello Vue!",
-      header: {
-        social: {
           ig: {
             url: "https://instagram.com/pnjstr.id",
             title: "panji",
