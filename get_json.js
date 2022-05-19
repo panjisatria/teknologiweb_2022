bb0c29144530238df2cda9c0ecb623ab09cd2280
Panji Satria Taqwa Putra Purnama <96733005+panjisatria@users.noreply.github.com>
2022-05-20 03:20:43 +0700

    Update get_json.js

--- get_json.js
+++ get_json.js
@@ -1,7 +1,7 @@
 Vue.createApp({
     data() {
       return {       
-        artikel: null, //variabel article
+        artikel: null, //variabel artikel
       };
     },
     methods: {
@@ -10,17 +10,31 @@ Vue.createApp({
         var converter = new showdown.Converter(); //mendefinisikan konverter
         axios
           .get(
-            "https://raw.githubusercontent.com//panjisatria/teknologiweb_2022/main/contents/artikel.html"
+            'https://raw.githubusercontent.com/panjisatria/teknologiweb_2022/main/contents/artikel.html'
           )
           .then((res) => {           
             var html = converter.makeHtml(res.data); //eksekusi konverter
-            this.article = html; //update variabel article
+            this.artikel = html; //update variabel artikel
           })
           .catch((error) => {
             console.log(error);
           });
       }
     },
+     getArticlesData() {
+      axios
+        .get(
+          'https://raw.githubusercontent.com/panjisatria/teknologiweb_2022/main/contents/artikel.html'
+      )
+        .then((res) => {
+          console.log(res.data);
+          this.articles = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
     beforeMount() {
       this.getMarkdownData()
     },
