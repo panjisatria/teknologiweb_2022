a46838f8fab1b2801278bc3bbc27721d28a88b3a
Panji Satria Taqwa Putra Purnama <96733005+panjisatria@users.noreply.github.com>
2022-05-20 03:37:35 +0700

    Update get_json.js

--- get_json.js
+++ get_json.js
@@ -10,7 +10,7 @@ Vue.createApp({
         var converter = new showdown.Converter(); //mendefinisikan konverter
         axios
           .get(
-            'https://raw.githubusercontent.com/panjisatria/teknologiweb_2022/main/contents/artikel.html'
+            'https://raw.githubusercontent.com/panjisatria/teknologiweb_2022/main/contents/markdown.html'
           )
           .then((res) => {           
             var html = converter.makeHtml(res.data); //eksekusi konverter
